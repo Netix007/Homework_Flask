1b55a9bd3f379632dca9533884a0b71aad716181
Alex <kino_mac@mail.ru>
2023-07-08 07:48:29 +0300

    Homework3

---
+++ Homework3/Task8/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=True, nullable=False)
+    last_name = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
